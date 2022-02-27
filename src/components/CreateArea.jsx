31350b7ada90346e6b1b190f6c9f3372d4485355
Winston Sevilla <winston_sevilla@outlook.com>
2022-02-27 14:08:51 +0800

    update

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,48 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
-function CreateArea(props) {
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    setNote(prevNote => {
-      return {
-        ...prevNote,
-        [name]: value
-      };
-    });
-  }
-
-  function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
-    event.preventDefault();
-  }
-
+function CreateArea() {
   return (
     <div>
-      <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <button onClick={submitNote}>Add</button>
+      <form>
+        <input name="title" placeholder="Title" />
+        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <button>Add</button>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 function Note(props) {
-  function handleClick() {
-    props.onDelete(props.id);
-  }
-
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button>DELETE</button>
     </div>
   );
 }
