5eaca375300090b010aca47636819345e168cce7
Winston Sevilla <winston_sevilla@outlook.com>
2022-02-27 13:39:02 +0800

    Challenge 2 Solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note"
-import Footer from "./Footer"
+import Note from "./Note";
+import Footer from "./Footer";
+import Notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {Notes.map(note => <Note key={note.key} title={note.title} content={note.content} />)}
             <Footer />
         </div>
     );
